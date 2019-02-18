b0f40e148bef939910c8f390cf8d151c12419906
Anthony Marques <anthony@armorica.gk>
2019-02-18 08:50:33 +0200

    installed firebase and added client.js file

---
+++ firebase-social/src/client.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCED5kxEKN2lD5C24qQDNH7dL3pzuIkbkU",
+  authDomain: "fir-social-886a6.firebaseapp.com",
+  databaseURL: "https://fir-social-886a6.firebaseio.com",
+  projectId: "fir-social-886a6",
+  storageBucket: "fir-social-886a6.appspot.com",
+  messagingSenderId: "707728488480"
+};
+firebase.initializeApp(config);
